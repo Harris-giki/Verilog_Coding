f7ac0c1a81cd824913e7e7e55b591a62fedefc54
harris-giki <harris-giki@gmail.com>
2025-05-15 11:59:20 +0500

    Arrays Initialized

--- Memories/Initializing_Arrays.v
+++ Memories/Initializing_Arrays.v
@@ -8,3 +8,11 @@ initial begin
     array_1d[0]=8'h11;
     array_1d[1]=8'h12;
     //similarly so on fill upto 16 locations
+
+    $display("1D array elements:");
+    for(i=0; i<16; i=i+1)
+    begin
+        $display("array_1d[%0d]=%h", i,array_1d[i]);
+    end
+end
+endmodule
